e4f953937e1b1416ee16326194317e13e98f0a6a
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-10-07 16:21:03 -0500

    feature: registrer screen added

---
+++ dittobox_mobile/lib/user_and_profile/ui/screens/register_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+  String userType = 'Worker';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 40),
+                const Text(
+                  'Register',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20, width: 20),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 80),
+                  child: SegmentedButton<String>(
+                    segments: const <ButtonSegment<String>>[
+
+                      ButtonSegment<String>(
+                        value: 'Worker',
+                        label: Text('Worker'),
+                        enabled: true,
+                      ),
+                      ButtonSegment<String>(
+                        value: 'Owner',
+                        label: Text('Owner'),
+                      ),
+                    ],
+                    selected: <String>{userType},
+                    onSelectionChanged: (Set<String> newSelection) {
+                      setState(() {
+                        userType = newSelection.first;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: !isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        if (userType == 'Owner') {
+                          //Navigator.pushNamed(context, AppRoutes.companyInfo);
+                        } else {
+                          // Navigator.pushNamed(context, AppRoutes.home);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.primaries.last,
+                        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      ),
+                      child: Text(userType == 'Owner' ? 'Continue' : 'Register'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, AppRoutes.login);
+                  },
+                  child: const Text('Already have an account? Login instead'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
