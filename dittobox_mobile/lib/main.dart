79f4f236a95403c2987a028d2771c25acd3db2a1
miikuru002 <jamutaqortegavelez@gmail.com>
2025-07-09 16:44:13 -0500

    fix: remove unused imports from main.dart

--- dittobox_mobile/lib/main.dart
+++ dittobox_mobile/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:ui';
 // import 'package:dittobox_mobile/firebase_options.dart';
 import 'package:dittobox_mobile/generated/l10n.dart';
 import 'package:dittobox_mobile/routes/app_routes.dart';
 // import 'package:dittobox_mobile/shared/infrastructure/data-sources/services/firebase_api.dart';
 import 'package:dittobox_mobile/styles/dittobox_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
